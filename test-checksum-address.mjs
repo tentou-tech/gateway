8e0b881e92b6e4f733703068bc3ea3470ae73ab3
kien <kien@tentou.tech>
2025-11-03 11:53:48 +0000

    Remove temporary test .mjs files

--- test-checksum-address.mjs
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'ethers';
-
-// Get properly checksummed address
-const address = '0x742d35Cc6634C0532925a3b844Bc9e7595f0bEb0';
-const checksummed = ethers.utils.getAddress(address.toLowerCase());
-
-console.log('Original:', address);
-console.log('Checksummed:', checksummed);
